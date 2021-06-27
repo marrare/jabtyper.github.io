ffc43d5d48b8a740cb718581292bd26b97f26c07
Rivaldo <rivaldo.alvesb36@gmail.com>
2021-06-27 17:59:10 -0300

    feat :sparkles: new version: jabTyperV2

--- js/indexedDB.js
+++ js/indexedDB.js
@@ -1,42 +1,64 @@
-$(document).ready(() => {
-    let db;
-    const request = window.indexedDB.open("jabtyper", 2);
+let db;
+const request = window.indexedDB.open("jabtyper", 2);
 
-    const frases = [
-        { dificuldade: 1, frase: "Testando isso aqui" },
-        { dificuldade: 2, frase: "Eu nem concordo nem discordo, muito pelo contrário" },
-        { dificuldade: 3, frase: "Eu nem concordo nem discordo, muito pelo contrário..." }
-    ];
+const frases = [
+    { dificuldade: 1, frase: "Água mole, pedra dura tanto bate até que fura." },
+    { dificuldade: 1, frase: "Tudo, antes de ser fácil, é difícil." },
+    { dificuldade: 1, frase: "Velhas convicções não levam a um novo lugar." },
+    { dificuldade: 2, frase: "Se você não sabe aonde quer chegar, nenhum vento lhe é favorável." },
+    { dificuldade: 2, frase: "Tem coisas que dão medo, mas não são perigosas, assim como tem coisas que são perigosas, mas não dão medo." },
+    { dificuldade: 2, frase: "Nada é mais fugaz do que a forma exterior, sua aparência muda como as flores do campo." },
+    { dificuldade: 3, frase: "Ela é de gêmeos, é mutável regida por Mercúrio. Ela é curiosa, loquaz, volátil, e adaptável como uma camaleoa. Rotina não é sua praia, aliás, ela também não gosta de praia." },
+    { dificuldade: 3, frase: "Entre um estimulo e uma resposta há um espaço, nesse espaço reside a nossa liberdade, e o nosso poder para escolher uma resposta, nessa resposta reside o potencial do nosso crescimento e bem estar." },
+    { dificuldade: 3, frase: "Sabendo o que sei e sabendo o que sabes e o que não sabes e o que não sabemos, ambos saberemos se somos sábios, sabidos ou simplesmente saberemos se somos sabedores." }
+];
 
-    request.onupgradeneeded = (event) => {
-        console.log("database updated");
-        db = event.target.result;
+request.onupgradeneeded = (event) => {
+    console.log("database updated");
+    db = event.target.result;
 
-        var fraseStore = db.createObjectStore("frases", { autoIncrement : true });
-        var pontuacaoStore = db.createObjectStore("pontuacoes", { autoIncrement : true });
+    let fraseStore = db.createObjectStore("frases", { autoIncrement : true });
+    let pontuacaoStore = db.createObjectStore("pontuacoes", { autoIncrement : true });
 
-        fraseStore.transaction.oncomplete = function(event) {
-            var clientesObjectStore = db.transaction("frases", "readwrite").objectStore("frases");
-            for (var i in frases) {
-                clientesObjectStore.add(frases[i]);
-            }
+    fraseStore.transaction.oncomplete = function(event) {
+        let frasesObjectStore = db.transaction("frases", "readwrite").objectStore("frases");
+        for (var i in frases) {
+            frasesObjectStore.add(frases[i]);
         }
-    };
-    request.onsuccess = (e) => {
-        console.log("database conected");
-        db = e.target.result;
-        getAllFrases();
     }
-    request.onerror = (e) => console.log(e.target.error);
+};
 
-    function getAllFrases() {
+request.onsuccess = (e) => {
+    console.log("database conected");
+    db = e.target.result;
+}
+request.onerror = (e) => console.log(e.target.error);
+
+function getAllFrases() {
+    return new Promise((resolve, reject) => {
         const fraseStore = db.transaction(["frases"]).objectStore("frases");
         const requestFrases = fraseStore.getAll();
 
         requestFrases.onsuccess = function(event) {
-            event.target.result.map((frase) => {
-                
-            })
-        };
-    }
-})
+            resolve(event.target.result);
+        }
+    });
+}
+
+function insertPontuacao(pontuacao) {
+    return new Promise((resolve, reject) => {
+        const pontuacoesObjectStore = db.transaction("pontuacoes", "readwrite").objectStore("pontuacoes");
+        pontuacoesObjectStore.add(pontuacao);
+    });
+}
+
+function getAllPontuacoes() {
+    return new Promise((resolve, reject) => {
+        const pontuacoesObjectStore = db.transaction(["pontuacoes"]).objectStore("pontuacoes");
+        const requestPontuacoes = pontuacoesObjectStore.getAll();
+
+        requestPontuacoes.onsuccess = function(event) {
+            resolve(event.target.result);
+        }
+    });
+}
--- js/main.js
+++ js/main.js
@@ -1,73 +1,170 @@
+let dificuldade;
+let userName;
+let timer;
+
+
 const loadPageGame = () => {
-    let nameValid = checkNameIsValid();
-    if(nameValid) {
+    dificuldade = $('input[name="dificuldade"]:checked').val();
+    userName = $("#name").val();
+
+    $("#name-valid").hide();
+    $("#dificuldade-valid").hide();
+    let valid = checkInputIsValid();
+    if(valid) {
         $("#block-name").slideUp();
+        setFrase();
         $("#block-game").slideDown(1000);
-        $("#name-valid").hide();
+        countdownToStart();
     } else {
         $("#name-valid").show();
     }
 }
 
-const checkNameIsValid = () => {
-    let name = $("#name").val();
-    if(name != "") return true;
-    else return false;
+const pressEnterLoadPage = (e) => {
+    if(e.keyCode === 13){
+        e.preventDefault();
+        loadPageGame();
+    }
+}
+
+const checkInputIsValid = () => {
+    let valid = true;
+    if(userName == "") {
+        valid = false;
+        $("#name-valid").show();
+    }
+    if(dificuldade == undefined) {
+        valid = false;
+        $("#dificuldade-valid").show();
+    }
+    return valid;
+}
+
+let countdownActual;
+function countdownToStart() {
+    countdownActual = 3;
+    const countTimer = setInterval(() => {
+        switch (countdownActual) {
+            case 3:
+                $("#block-count-start").show();
+                $("#countThree").show();
+                countdownActual = 2;
+                break;
+            case 2:
+                $("#countThree").hide();
+                $("#countTwo").show();
+                countdownActual = 1;
+                break;
+            case 1:
+                $("#countTwo").hide();
+                $("#countOne").show();
+                countdownActual = 0;
+                break;
+            case 0:
+                $("#countOne").hide();
+                $("#block-count-start").hide();
+                $("#block-game-text").slideDown(1000);
+                setTimeout(() => startTimerGamer(), 1000);
+                clearInterval(countTimer);
+                break;
+        }
+    }, 1000);
+    
+}
+
+const setFrase = async () => {
+    const frases = await getAllFrases();
+    const frasePorDificuldade = frases.filter((frase) => { return frase.dificuldade == dificuldade; });
+    const fraseSelecionada = frasePorDificuldade[Math.floor(Math.random() * frasePorDificuldade.length)];
+    
+    $("#frase").text(fraseSelecionada.frase);
+    checarPalavras();
 }
 
 //Game
 var campo = $("#digitacao");
 campo.on("input", function() {
-    var frase = campo.val();
-    $("#caracteres-digitados").text(frase.length);
-
-    var palavras = frase.split(" ");
-    var numPalavras = palavras.reduce((num, palavra) => {
-        if(palavra != "") num++;
-        return num;
-    }, 0);
-    $("#palavras-digitadas").text(numPalavras);
+    checarPalavras();
 });
+function checarPalavras() {
+    let fraseResposta = campo.val();
+    $("#caracteres-digitados").text(fraseResposta.length);
+
+    let palavrasBase = $("#frase").text().split(" ");
+    let palavrasResposta = fraseResposta.split(" ");
+    const palavrasCorretas = palavrasBase.reduce((num, palavraBase, index) => {
+        return num += palavrasResposta[index] == palavraBase ? 1 : 0;
+    }, 0);
+    $("#palavras-digitadas").text(`${palavrasCorretas}/${palavrasBase.length}`);
+
+    if(palavrasCorretas == palavrasBase.length) finishGame();
+}
 
 // Timer
-const tempoInicial = 15;
-campo.on("focus", function() {
+let tempoInicial = 0; //time in miliseconds
+function setTime() {
+    switch (dificuldade) {
+        case "1":
+            tempoInicial = 300;
+            break;
+        case "2":
+            tempoInicial = 250;
+            break;
+        case "3":
+            tempoInicial = 200;
+            break;
+    }
+}
+function startTimerGamer() {
+    setTime();
     let tempoRestante = tempoInicial;
-    var timer = setInterval(function() {
+    campo.focus();
+    timer = setInterval(function() {
         let tempPercent = (tempoRestante/tempoInicial) * 100;
         $('.progress-bar').css('width', tempPercent+'%').attr('aria-valuenow', tempPercent);
-        $("#tempo").text(tempoRestante);
+        $("#tempo").text(tempoRestante/10);
 
         if(tempoRestante == 0) { 
-            campo.attr("disabled",true);
-            clearInterval(timer);
-            sendScore();
+            finishGame();
         } else {
             tempoRestante--;
         }
-    }, 1000);
-})
+    }, 100);
+}
+
+function finishGame() {
+    campo.attr("disabled",true);
+    clearInterval(timer);
+    sendScore();
+}
+
 
 //Reset
 const resetGame = () => {
     $("#block-score").slideUp();
     $("#block-name").slideDown(1000);
-    
+    dificuldade = undefined;
+    userName == "";
     campo.attr("disabled", false);
     $("#name").val("");
     campo.val("");
     $("#caracteres-digitados").text("0");
     $("#palavras-digitadas").text("0");
-    $("#tempo").text(tempoInicial);
+    $("#tempo").text(tempoInicial/10);
     $('.progress-bar').css('width', '100%').attr('aria-valuenow', '100');
 }
 
 //Score
-function sendScore() {
+async function sendScore() {
     $("#block-game").slideUp();
+    $("#block-game-text").slideUp();
+    $('#block-score').css('display', 'flex');
     $("#block-score").slideDown(1000);
+    insertPontuacao({ userName: $("#name").val(), pontuacao: $("#palavras-digitadas").text() });
+    const pontuacoes = await getAllPontuacoes();
 
-    var name = $("#name").val();
-    var palavras = $("#palavras-digitadas").text();
-    $("#score").append(`<tr><td>${name}</td><td>${palavras}</td></tr>`);
+    $("#score").html("");
+    pontuacoes.map((pontuacao) => {
+        $("#score").append(`<tr><td>${pontuacao.userName}</td><td>${pontuacao.pontuacao}</td></tr>`);
+    });
 }
